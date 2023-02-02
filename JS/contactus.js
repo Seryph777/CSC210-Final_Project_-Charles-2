12219c75904a223d7f6b65a02582d77173da8d6b
Seryph777 <charlesxgrant11@gmail.com>
2023-02-02 14:24:40 -0700

    Update contactus.js

--- JS/contactus.js
+++ JS/contactus.js
@@ -3,5 +3,5 @@ var quote = document.getElementById("daily_quote")
 async function getQuote(url="https://theysaidso.com"){
     let quoteObject = await fetch(url="https://theysaidso.com");
     let quoteText = (await quoteObject).text();
-    quote.innerHTML = quoteText
+    document.getElementById("daily_quote").innerText = quoteText;
 }
