7888585b1c1323cf9a808842f720509bcf1a95fe
Entity-903 <charlesmassie021@gmail.com>
2023-02-02 14:16:50 -0700

    Update contactus.js

--- JS/contactus.js
+++ JS/contactus.js
@@ -1,2 +1,7 @@
 var quote = document.getElementById("daily_quote")
 
+async function getQuote(url="https://theysaidso.com"){
+    let quoteObject = await fetch(url="https://theysaidso.com");
+    let quoteText = (await quoteObject).text();
+    quote.innerHTML = quoteText
+}
