412f288b9fa94006a10d905777fc8d40f36e2974
Seryph777 <charlesxgrant11@gmail.com>
2023-02-02 16:04:09 -0700

    Finished API Call

--- JS/contactus.js
+++ JS/contactus.js
@@ -1,7 +1,17 @@
+fetch("http://quotes.rest/qod")
+.then((response) => response.json())
+.then((data) => {
+    console.log("Check")
+    var quote = document.getElementById("daily_quote")
+    var quoteIMG = document.getElementById("quote_img")
+    quote.textContent = data.contents.quotes[0].quote
+    quoteIMG.src = data.contents.quotes[0].background
+})
+
 var quote = document.getElementById("daily_quote")
 
 async function getQuote(url="https://theysaidso.com"){
     let quoteObject = await fetch(url="https://theysaidso.com");
     let quoteText = (await quoteObject).text();
-    document.getElementById("daily_quote").innerText = quoteText;
+    document.getElementById("daily_quote").innerHTML = quoteText
 }
